212b9e0463c19966e585128569da937207147336
Pavel Cerny <cerny.pav@gmail.com>
2017-03-01 12:56:22 +0100

    hello world html template

--- chat_demo/settings.py
+++ chat_demo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chat.apps.ChatConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
