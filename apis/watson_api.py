23f9246368b76aeef4426d9a27b5057148c4118c
Pavel Cerny <cerny.pav@gmail.com>
2017-05-19 00:32:47 +0200

    upgraded watson

--- apis/watson_api.py
+++ apis/watson_api.py
@@ -1,9 +1,18 @@
 from watson_developer_cloud import ConversationV1
 
+# conversation = ConversationV1(
+#   username='8df4159f-bfda-48f4-9827-c331200caebd',
+#   password='lpULJnXdW2mx',
+#   version='2017-02-03'
+# )
+#
+# WORKSPACE_ID = '225b2116-9727-4572-8fd4-4e4c1c69a0ed'
+
+
 conversation = ConversationV1(
   username='8df4159f-bfda-48f4-9827-c331200caebd',
   password='lpULJnXdW2mx',
-  version='2017-02-03'
+  version='2017-04-21'
 )
 
-WORKSPACE_ID = '225b2116-9727-4572-8fd4-4e4c1c69a0ed'
+WORKSPACE_ID = '563a1de2-b88c-42ea-beea-f8435f82a60e'
