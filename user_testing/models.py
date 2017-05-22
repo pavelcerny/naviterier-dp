951fb929c3ca54f5f0f1f20ccc9e9e8758710560
Pavel Cerny <cerny.pav@gmail.com>
2017-05-22 09:00:21 +0200

    fixed recording game

--- user_testing/models.py
+++ user_testing/models.py
@@ -32,7 +32,7 @@ class Experiment(models.Model):
 class GpsCoords(models.Model):
     lat = models.FloatField(default=0)
     lon = models.FloatField(default=0)
-    experiment = Experiment
+    experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE)
 
     class Meta:
         abstract = True
