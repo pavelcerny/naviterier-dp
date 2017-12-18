7380c73f756d8f5f0c7ecb5dbd5bbbf935c5060c
Pavel Cerny <cerny.pav@gmail.com>
2017-12-18 12:32:14 +0100

    watson comments

--- apis/watson_api.py
+++ apis/watson_api.py
@@ -1,9 +1,12 @@
 from watson_developer_cloud import ConversationV1
 
+# for IBM Watson Conversation https://watson-conversation.ng.bluemix.net
+
+
+# Credentials for POI prototype
 conversation = ConversationV1(
   username='8df4159f-bfda-48f4-9827-c331200caebd',
   password='lpULJnXdW2mx',
   version='2017-04-21'
 )
-
 WORKSPACE_ID = '563a1de2-b88c-42ea-beea-f8435f82a60e'
