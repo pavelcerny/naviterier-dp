64e2b4923b279a9210f92b12b5c435e41822a3e6
Pavel Cerny <cerny.pav@gmail.com>
2017-12-18 12:48:18 +0100

    google api's comments

--- apis/google_api.py
+++ apis/google_api.py
@@ -2,10 +2,23 @@ import json
 
 import requests
 
+
+# for Google API https://developers.google.com/maps/documentation/
+
+
+# credentials
 GOOGLE_API_KEY = "AIzaSyC7WCaTEPswl2Cs77ncqxiz7O4fLK2z2Wk"
 URL = "https://maps.googleapis.com/maps/api/geocode/json"
 
+
 def getAddress(lat, lon):
+    """
+    Returns Address for given latitude and longitude. The format of the address 'streetName houseNumber'
+
+    >>>getAddress(50.0777201, 14.416807)
+    'Myslíkova 13'
+    """
+
     # request google API
     payload = {'latlng': str(lat) + "," + str(lon),  # 50.094265,
                'key': GOOGLE_API_KEY}
@@ -28,12 +41,21 @@ def getAddress(lat, lon):
 
 
 def getGps(address):
+    """
+    Returns lat & lon for given address.
+
+    >>>getGPS('Myslíkova 187/13')
+    {'lat' : 50.0777201, 'lon' : 14.416807}
+    >>>getGPS('Myslíkova 13')
+    {'lat' : 50.0777201, 'lon' : 14.416807}
+    """
+
     # request google API
     payload = {'address': address}
     url = URL
     r = requests.get(url, params=payload)
     data = r.text
-    # parse json data
+    
     # EXAMPLE RESPONSE
     #     {
     #     "results": [
@@ -54,6 +76,8 @@ def getGps(address):
     #     ],
     #     "status": "OK"
     # }
+
+    # parse json data
     data = json.loads(data)
     # get object with "lat" and "lng"
     location = data['results'][0]['geometry']['location']
