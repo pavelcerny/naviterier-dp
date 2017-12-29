964800d8a462c7c5a5245af52c3c275be17e6a0e
Pavel Cerny <cerny.pav@gmail.com>
2017-12-29 16:38:18 +0100

    moved watsonResponse url to APIs urls

--- chat/urls.py
+++ chat/urls.py
@@ -30,7 +30,6 @@ urlpatterns = [
     # APIs
     url(r'^process$', views.processUserInput, name='process_request'),
     url(r'^address-from-gps$', apis.views.getAddressFromGpsAPI, name='get_address'),
-    url(r'^conversation$', apis.views.watsonResponse, name='watson_response'),
     url(r'^googlegeocoding$', apis.views.getGpsFromAddressAPI, name='google_geo'),
 
 
